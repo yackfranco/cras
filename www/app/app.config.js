2efd8ff757710efe40c891f65ffdba1bb4864b8e
Darkjhonf <jhonsepulveda41@gmail.com>
2017-05-02 17:17:47 -0500

    Se Crea el Formulario principal del aplicativo Se agrega configuracion, Se Modifica el estilo (css) Se Agregan imagenes

--- www/app/app.config.js
+++ www/app/app.config.js
@@ -1,19 +1,23 @@
 angular.module('IMPERIUM').
         config(['$routeProvider',
-            function config($routeProvider) {
-                $routeProvider.
-                        when('/', {
-                            controller: 'loginController',
-                            templateUrl: 'app/template/login.html'
-                        }).
-                        when('/ces', {
-                            controller: 'cesController',
-                            templateUrl: 'app/template/ces.html'
-                        }).
-                        when('/copyRight', {
-                            controller: 'copyrightController',
-                            templateUrl: 'app/template/copyRight.html'
-                        }).
-                        otherwise('/');
-            }
+          function config($routeProvider) {
+            $routeProvider.
+                    when('/', {
+                      controller: 'loginController',
+                      templateUrl: 'app/template/login.html'
+                    }).
+                    when('/ces', {
+                      controller: 'cesController',
+                      templateUrl: 'app/template/ces.html'
+                    }).
+                    when('/copyRight', {
+                      controller: 'copyrightController',
+                      templateUrl: 'app/template/copyRight.html'
+                    }).
+                    when('/menuPrincipal', {
+                      controller: 'menuPrincipalController',
+                      templateUrl: 'app/template/menuPrincipal.html'
+                    }).
+                    otherwise('/');
+          }
         ]);
