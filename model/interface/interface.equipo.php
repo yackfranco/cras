aa453d0bfef5837ba0df7b56e5276de38d56ea31
Hefesto12 <kevinhernandez.92@hotmail.com>
2017-05-04 17:38:14 -0500

    Se creo el DAO de la tabla ces_equipo en la carpeta model: - class.equipoDAO.php - class.equipoDAOExt.php - interface.equipo.php - table.equipo.php

---
+++ model/interface/interface.equipo.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * metodo para la tabla equipo
+ */
+interface IEquipo {
+
+  /**
+   * metodo para seleccionar todos los registros de la tabla
+   */
+  public function select();
+  /**
+   * metodo para seleccionar un registro especifico de la BD
+   * @param integer $id
+   */
+
+  public function selectById(integer $id);
+  /**
+   * metodo para insertar un equipo en la BD
+   * @param equipo $equipo
+   */
+
+  public function insert(equipo $equipo);
+  /**
+   * metodo para actualizar un registro por ID en la BD
+   * @param equipo $equipo
+   */
+
+  public function update(equipo $equipo);
+  /**
+   * metodo para el borrado fisico o logico de un registro en la BD
+   * @param integer $id
+   * @param boolean $logico
+   */
+
+  public function delete(integer $id, boolean $logico = true);
+}
+
