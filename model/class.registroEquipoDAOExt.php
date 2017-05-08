1e7d74f5833d48979a439bd0e15a83e2bb404f52
juandiegorb <juan.drb.99@gmail.com>
2017-05-08 13:30:24 -0500

    Se completa el DAO, la interfaz, la tabla y la extencion del DAO del registro Equipo

--- model/class.registroEquipoDAOExt.php
+++ model/class.registroEquipoDAOExt.php
@@ -1,8 +1,16 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class registroEquipoDAOExt extends registroEquipoDAO {
 
+  public function search($id) {
+    $sql = 'SELECT reg_equip_id, equi_id, reg_per_id, fecha_entrada, fecha_salida FROM ces_registro_equipo WHERE fecha_entrada=:entrada AND fecha_salida=:salida';
+    $params = array(
+
+    ':id' => $id,
+    ':entrada' => $reEquipo->getEntrada(),
+    ':salida' => $reEquipo->getSalida(),
+    );
+    return $this->query($sql, $params);
+  }
+
+}
