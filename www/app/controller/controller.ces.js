84b90594c4c39efbc3054376b9950d45ac36299e
yackfranco <jesusandres548@gmail.com>
2017-06-13 15:43:37 -0500

    Se realiza el Control de Entrada y salida

--- www/app/controller/controller.ces.js
+++ www/app/controller/controller.ces.js
@@ -1,5 +1,5 @@
-angular.module('IMPERIUM').controller('cesController', ['$scope', 'cesServices', '$location', '$timeout', function ($scope, cesServices, $location, $timeout) {
-
+angular.module('IMPERIUM').controller('cesController', ['$scope', 'cesServices', '$location', '$timeout','rolAdmin','$sessionStorage', function ($scope, cesServices, $location, $timeout,rolAdmin,$sessionStorage) {
+$scope.focus = true;
     $scope.alertaEntrada = false;
     $scope.alertaSalida = false;
     $scope.RegistroEquipo = false;
@@ -13,20 +13,25 @@ angular.module('IMPERIUM').controller('cesController', ['$scope', 'cesServices',
         $scope.identificacion = "";
 
         if (respuesta.data.accion == 'inserto') {
+          $tiempo = 1000;
           //Entrada
           $scope.nombre = (respuesta.data.persona[0].per_nombre) + " " + (respuesta.data.persona[0].per_apellidos);
           $scope.horaE = respuesta.data.ultimoRegistro[0].reg_per_entrada;
           $scope.ficha = respuesta.data.persona[0].per_ficha
           $scope.horaS = "";
+          $scope.foto = respuesta.data.persona[0].per_foto;
           $scope.alertaEntrada = true;
           $scope.alertaSalida = false;
+          
 
         } else if (respuesta.data.accion == 'actualizo') {
+          $tiempo = 1000;
           //Salida
           $scope.nombre = (respuesta.data.persona[0].per_nombre) + " " + (respuesta.data.persona[0].per_apellidos);
           $scope.horaE = respuesta.data.ultimoRegistro[0].reg_per_entrada;
           $scope.horaS = respuesta.data.ultimoRegistro[0].reg_per_salida;
           $scope.ficha = respuesta.data.persona[0].per_ficha;
+          $scope.foto = respuesta.data.persona[0].per_foto;
           $scope.alertaEntrada = false;
           $scope.alertaSalida = true;
 
@@ -54,4 +59,15 @@ angular.module('IMPERIUM').controller('cesController', ['$scope', 'cesServices',
       $scope.RegistroEquipo = true;
       $location.path("/registrarEquipo");
     }
+    
+    $scope.salir = function(){
+      console.log("Hola Amor  ");
+//      console.log($sessionStorage.usuario.rol_id);
+      if($sessionStorage.usuario.rol_id == rolAdmin ){
+        $location.path("/menuPrincipal");
+      }else{
+        $location.path("/logout");
+      }
+    }
+    
   }]);
