79aff9ed88bfbeb4ee13c63e9ac42cde31f26770
yackfranco <jesusandres548@gmail.com>
2017-06-24 17:34:21 -0500

    Se termino el modulo de Control de Entrada y Salida del Personal y Objetos correccion de pequeños fallos
    Colaboradores:
    -Jesus Andres Gonzalez
    -Brandon Morales
    
    tester:
    -kevin Hernandez

--- www/app/controller/controller.ces.js
+++ www/app/controller/controller.ces.js
@@ -21,8 +21,6 @@ angular.module('IMPERIUM').controller('cesController', ['$scope', 'cesServices',
     $interval(callAtInterval, 1000);
 
 
-
-
     $scope.entradaEquipo = function (x) {
       x.reg_per_id = $reg_per_id;
       x.per_id = $idPersona;
@@ -38,6 +36,7 @@ angular.module('IMPERIUM').controller('cesController', ['$scope', 'cesServices',
       });
     }
 
+
     //funcion del reloj
     function callAtInterval() {
       $h = $filter('date')(new Date(), 'yyyy-MM-dd HH:mm:ss');
@@ -47,7 +46,6 @@ angular.module('IMPERIUM').controller('cesController', ['$scope', 'cesServices',
 //llena la tabla de CES de equipos
     function traerTablaEquipos() {
       cesServices.cesPc({per_id: $idPersona, accion: 'llenarTabla'}).then(function successCallback(respuesta) {
-        console.log(respuesta);
         if (respuesta.data.accion == "llenarTabla") {
           $scope.mostrarTabla = true;
           $scope.tabla = respuesta.data.objeto;
@@ -62,41 +60,37 @@ angular.module('IMPERIUM').controller('cesController', ['$scope', 'cesServices',
 
 //llena los datos de la persona cuando viene del modulo registrarEquipo
     if ($sessionStorage.datosPersona) {
+
       $scope.ShowFoto = true;
       $scope.nombre = $sessionStorage.datosPersona.nombre;
       $scope.horaE = $sessionStorage.datosPersona.he;
       $scope.ficha = $sessionStorage.datosPersona.ficha;
       $scope.horaS = $sessionStorage.datosPersona.hs;
-//      console.log();
       $scope.foto = $sessionStorage.datosPersona.foto;
       $scope.identificacion2 = $sessionStorage.datosPersona.id;
       $idPersona = $sessionStorage.idPersona;
-
       $scope.btnSalida = false;
       traerTablaEquipos();
-//      if ($sessionStorage.datosPersona.accion == "cargarRegistros") {
-        $scope.btnRegistrarEquipo = false;
-//      }
+      $scope.btnRegistrarEquipo = false;
       delete $sessionStorage.idPersona;
+      $reg_per_id = $sessionStorage.regPerId;
       delete $sessionStorage.regPerId;
       delete $sessionStorage.datosPersona;
+
     }
 
 
     //funcion que realiza el control de entrada y salida del personal
     $scope.consultarDocumento = function () {
       cesServices.validarExistencia({id: $scope.identificacion}).then(function successCallback(respuesta) {
-        console.log(respuesta.data.accion);
-        console.log(respuesta.data);
         $scope.identificacion2 = $scope.identificacion;
         $scope.identificacion = "";
-        $idPersona = respuesta.data.persona[0].per_id;
-//        $dirfoto = respuesta.data.persona[0].
         if (respuesta.data.accion == 'inserto') {
-          $tiempo = 1000;
+
           //Entrada
+          $idPersona = respuesta.data.persona[0].per_id;
+          $tiempo = 1000;
           $scope.nombre = (respuesta.data.persona[0].per_nombre) + " " + (respuesta.data.persona[0].per_apellidos);
-          console.log(respuesta.data.ultimoRegistro[0].reg_per_entrada);
           $scope.horaE = moment(respuesta.data.ultimoRegistro[0].reg_per_entrada, 'YYYY-MM-DD H:mm:ss').format('H:mm:ss');
           $scope.ficha = respuesta.data.persona[0].per_ficha
           $scope.horaS = "";
@@ -113,15 +107,15 @@ angular.module('IMPERIUM').controller('cesController', ['$scope', 'cesServices',
           $scope.btnSalida = false;
 
         } else if (respuesta.data.accion == 'actualizo') {
-          $tiempo = 1000;
-          //Salida
 
+          //Salida
+          $idPersona = respuesta.data.persona[0].per_id;
+          $tiempo = 1000;
           $scope.nombre = (respuesta.data.persona[0].per_nombre) + " " + (respuesta.data.persona[0].per_apellidos);
           $scope.horaE = moment(respuesta.data.ultimoRegistro[0].reg_per_entrada, 'YYYY-MM-DD H:mm:ss').format('HH:mm:ss');
           $scope.horaS = moment(respuesta.data.ultimoRegistro[0].reg_per_salida, 'YYYY-MM-DD H:mm:ss').format('HH:mm:ss');
           $scope.ficha = respuesta.data.persona[0].per_ficha;
           $reg_per_id = respuesta.data.ultimoRegistro[0].reg_per_id;
-//          $scope.foto = "app/pictures/"+respuesta.data.persona[0].per_foto;
           $scope.ShowFoto = true;
           if (respuesta.data.persona[0].per_foto) {
             $scope.foto = "app/pictures/" + respuesta.data.persona[0].per_foto;
@@ -132,13 +126,16 @@ angular.module('IMPERIUM').controller('cesController', ['$scope', 'cesServices',
           $scope.alertaSalida = true;
           $scope.btnRegistrarEquipo = true;
           $scope.btnSalida = true;
+
         } else {
+
           //la persona no existe
           $sessionStorage.savePersonFromCes = true;
           $scope.alertaEntrada = false;
           $scope.alertaSalida = false;
           $location.path("/registroPersonal");
           $scope.ShowFoto = false;
+
         }
         traerTablaEquipos();
         $timeout(function () {
@@ -147,9 +144,10 @@ angular.module('IMPERIUM').controller('cesController', ['$scope', 'cesServices',
         }, $tiempo);
 
       }, function errorCallback(answer) {
-
+        console.error(answer);
       });
     };
+
 
     //pregunta la cual se hace para realizar el control despues de guardar una persona
     if ($sessionStorage.registroCreado) {
@@ -159,47 +157,35 @@ angular.module('IMPERIUM').controller('cesController', ['$scope', 'cesServices',
     }
 
 
-
-
-
-
     $scope.salidaPc = function (x) {
-//      console.log(x.reg_equi_id);
       cesServices.cesPc({accion: 'Salida', id: x.reg_equi_id}).then(function successCallback(respuesta) {
         if (respuesta.data.accion == "ObjetoSalio") {
           traerTablaEquipos();
         }
       }, function errorCallback(respuesta) {
-
+        console.error(answer);
       });
     }
+
 
 //funcion que muestra el campo para realizar el control de entrada y salida de equipo
     $scope.registrarEquipo = function () {
       $scope.RegistroEquipo = true;
     }
 
+
 //funcion que realiza el control de entrada y salida de los equipos
     $scope.registrarPc = function () {
       cesServices.cesPc({serial: $scope.registroEquipo, reg_per_id: $reg_per_id, accion: 'ces'}).then(function successCallback(respuesta) {
-        console.log(respuesta);
         //se pregunta que si el pc existe
         if (respuesta.data.accion == "noExistePc") {
           $scope.nuevoObjeto();
-
-
-
         } else if (respuesta.data.accion == "bienesEntrar") {
-//          if (respuesta.data.filas == 200) {
           $('#modalEquipo').modal('show');
           $scope.objeto = respuesta.data.objeto;
-
-//          } else {
-//            $location.path("/registrarEquipo");
-//          }
         }
       }, function errorCallback(respuesta) {
-        console.log(respuesta);
+        console.error(respuesta);
       });
     }
 
@@ -220,5 +206,4 @@ angular.module('IMPERIUM').controller('cesController', ['$scope', 'cesServices',
       $('#modalEquipo').modal('hide');
       $location.path("/registrarEquipo");
     }
-
   }]);
